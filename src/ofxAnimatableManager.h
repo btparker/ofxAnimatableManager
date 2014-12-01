acd05cb6060c861f3b03d9f93c1801aac824ba3d
Tyler Parker <tylerparker@localprojects.net>
2014-12-01 00:31:10 -0500

    I can haz delayz?

--- src/ofxAnimatableManager.h
+++ src/ofxAnimatableManager.h
@@ -15,8 +15,14 @@ namespace AnimatableTypes {
     enum ENUM{FLOAT, COLOR, POINT};
 }
 
+namespace AnimationAlignments {
+    enum ENUM{START, END};
+}
+
 struct AnimatableValues{
     int durationUnits = -1;
+    int delayUnits = -1;
+    AnimationAlignments::ENUM alignAnimation = AnimationAlignments::START;
     AnimatableTypes::ENUM type;
     AnimCurve curveStyle;
     
@@ -33,6 +39,7 @@ struct State{
     float duration = -1.0;
     float durationUnitLength = -1;
     int durationUnits = -1;
+    int delayUnits = -1;
     
     AnimCurve curveStyle;
     
@@ -41,31 +48,33 @@ struct State{
 
 
 class ofxAnimatableManager: public ofxAnimatableFloat{
-    public:
-        ofxAnimatableManager();
-        ~ofxAnimatableManager();
-        void update(float dt);
-        void setCurve( AnimCurve curveStyle );
-        void addAnimatable(ofxAnimatable* animatable, string key = "");
-        void setDurationUnits(string key, int units);
-        void updateDurations();
-        void setDuration(float duration);
-        void startStateDefinition(string state);
-        void endStateDefinition(string state);
-        void startStateTransitionDefinition(string state1, string state2);
-        void endStateTransitionDefinition(string state1, string state2);
-        void animateTo(string key, ofPoint value, int durationUnits);
-        void animateTo(string key, float value, int durationUnits);
-        void animateTo(string key, ofColor value, int durationUnits);
-        void animateState(string key);
-
-    protected:
-        string activeState;
-        map<string, ofxAnimatable*> animatables;
-        map<string, State> states;
-        map<string, int> animatableDurationUnits;
+public:
+    ofxAnimatableManager();
+    ~ofxAnimatableManager();
+    void update(float dt);
+    void setCurve( AnimCurve curveStyle );
+    void addAnimatable(ofxAnimatable* animatable, string key = "");
+    void setDurationUnits(string key, int units);
+    void setDelayUnits(string key, int units, AnimationAlignments::ENUM alignAnimation = AnimationAlignments::START);
+    void updateDurations();
+    void setDuration(float duration);
+    void startStateDefinition(string state);
+    void endStateDefinition(string state);
+    void startStateTransitionDefinition(string state1, string state2);
+    void endStateTransitionDefinition(string state1, string state2);
+    void animateTo(string key, ofPoint value, int durationUnits, int delayUnits = -1, AnimationAlignments::ENUM alignAnimation = AnimationAlignments::START);
+    void animateTo(string key, float value, int durationUnits, int delayUnits = -1, AnimationAlignments::ENUM alignAnimation = AnimationAlignments::START);
+    void animateTo(string key, ofColor value, int durationUnits, int delayUnits = -1, AnimationAlignments::ENUM alignAnimation = AnimationAlignments::START);
     
-    private:
+    void animateState(string key);
     
-        void startAfterWait();
+protected:
+    string activeState;
+    map<string, ofxAnimatable*> animatables;
+    map<string, State> states;
+    map<string, int> animatableDurationUnits;
+    
+private:
+    
+    void startAfterWait();
 };
